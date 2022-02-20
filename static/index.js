f04f455f7d7ef3e1577993df52bbe08406c68e58
Mtdts0521 <98327854+Mtdts0521@users.noreply.github.com>
2022-02-20 20:52:36 +0900

    Update index.js

--- static/index.js
+++ static/index.js
@@ -343,11 +343,11 @@ function shareText(score) {
         return '倒计时多了' + ((deviation_time / 1000) - 20).toFixed(2) + "s";
     }
     SubmitResults();
-    if (score <= 49) return '试着好好练一下？';
-    if (score <= 99) return 'TCL';
-    if (score <= 149) return 'TQL';
-    if (score <= 199) return '您';
-    return '人？';
+    if (score <= 49) return '小极很生气！';
+    if (score <= 99) return '好像不如啵啵';
+    if (score <= 149) return '快要追上我了';
+    if (score <= 199) return '祝小极中孝顺利！';
+    return '公司脸呢？';
 }
 
 function toStr(obj) {
